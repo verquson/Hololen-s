bf1befee896e66839bf12a1e73bb1ae595dc20e2
Markka200 <81672997+Markka200@users.noreply.github.com>
2023-03-08 10:24:37 +0200

    Nappuloita

--- Assets/LabelCreator.cs
+++ Assets/LabelCreator.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class LabelCreator : MonoBehaviour
 {
     public GameObject Prefab;
+    public GameObject ExclamationMarkPrefab;
+    public GameObject QuestionMarkPrefab;
     public float Scale = 0.1f;
     public string Text = "Label";
     public float FontSize = 36;
@@ -25,5 +27,31 @@ public class LabelCreator : MonoBehaviour
         Label.GetComponent<TMP_Text>().text = Text;
         Label.GetComponent<TMP_Text>().fontSize = FontSize;
         Label.GetComponent<TMP_Text>().color = Color;
+    }
+    public void CreateQuestionMark()
+    {
+        GameObject QuestionMark = Instantiate(QuestionMarkPrefab, Parent);
+        QuestionMark.transform.position = Position.transform.position;
+        Vector3 temp = QuestionMark.GetComponent<Transform>().localScale;
+        temp.x = Scale ;
+        temp.y = Scale ;
+        temp.z = Scale ;
+        QuestionMark.GetComponent<Transform>().localScale = temp;
+
+    }
+    public void CreateExclamationMark()
+    {
+        GameObject ExclamationMark = Instantiate(ExclamationMarkPrefab, Parent);
+        ExclamationMark.transform.position = Position.transform.position;
+        Vector3 temp = ExclamationMark.GetComponent<Transform>().localScale;
+        temp.x = Scale ;
+        temp.y = Scale ;
+        temp.z = Scale ;
+        ExclamationMark.GetComponent<Transform>().localScale = temp;
+    }
+
+    public void UI()
+    {
+        //tähän se miten näkyy millasta ollaan luomassa ennen sen luomista
     }
 }
